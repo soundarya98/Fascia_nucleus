e6c743d677f7bd6adf1231d5df1d39dcf050081b
Walaa alkhanaizi <walaa@Walaas-MacBook-Pro.local>
2020-03-24 17:41:24 -0700

    merge conflict resolve

--- Firmware/development/Fascia_collect_sensor_data/WiFi_Settings.h
+++ Firmware/development/Fascia_collect_sensor_data/WiFi_Settings.h
@@ -4,9 +4,17 @@
 
 // #define SECRET_SSID "raspi_wifi"
 // #define SECRET_PASS "fluidfluid"
+<<<<<<< Updated upstream
 #define SECRET_SSID "Rotaryguru2"
 #define SECRET_PASS "rx7turbo2"
 #define HOST_ID     "10.0.0.242" /*"192.168.0.101"*/
+=======
+//#define SECRET_SSID "gitgudbruh"
+//#define SECRET_PASS "giganticorchestra203"
+#define SECRET_SSID "#8103"
+#define SECRET_PASS "1423qweasd"
+#define HOST_ID     "10.0.0.77"/*"192.168.0.13" /*"192.168.0.101"*/
+>>>>>>> Stashed changes
 #define PORT_NUM    8899
 
 #define SEND_SIZE 19
